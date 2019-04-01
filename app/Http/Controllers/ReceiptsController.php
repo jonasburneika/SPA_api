af3383890e4a01f4dfef995de1d93ff216474074
Jonas <Jonas.burneika@gmail.com>
2019-04-01 21:05:29 +0300

    added user points increasment method

--- app/Http/Controllers/ReceiptsController.php
+++ app/Http/Controllers/ReceiptsController.php
@@ -23,22 +23,46 @@ class ReceiptsController extends Controller
             'number'    => 'required|unique:receipts,number',
             'user_id'   => 'required|integer'
         ]);
-
+        
         if ($validator->fails()) {
            return $validator->errors();
         } else {
+            $userId = $request->user_id;
+            $user = User::find($userId);
+            if (!$user){
+                return ['error'=>'User with given ID doesn\'t exist!']; 
+            }
             $receipt = new Receipts;
             $receipt->number = $request->number;
-            $receipt->user_id = $request->user_id;
+            $receipt->user_id = $userId;
             $receipt->save();
             $receiptId = $receipt->id;
-            $logId = Temp::toLog($receiptId, $request->user_id);
+            $logId = Temp::toLog($receiptId, $userId, 0);
             if (!is_int($logId)){
                Receipts::find($receiptId)->delete();
-               return ['error'=>'Something goes wrong with LOG Method, please contact support and try again later'];
+               return ['error'=>'Something goes wrong with LOG Method, please contact support and try again later!'];
             } else {
-                return $logId;
+                $points = $this->getPointsAmount($userId);
+                if ($user){
+                    $user->points += $points;
+                    $user->update();
+                    $logId = Temp::toLog($receiptId, $userId, 1);
+                    return [
+                        'userId'=>$user->id,
+                        'points'=>$user->points,
+                    ];
+                }
             }
+        }
+    }
+
+    public function getPointsAmount($userId)
+    {
+        $receipts = Receipts::where('user_id',$userId)->where('created_at', '>=', date('Y-m-d').' 00:00:00')->count();
+        if ($receipts == 3) {
+            return 30;
+        } else {
+            return 5;
         }
     }
 }
