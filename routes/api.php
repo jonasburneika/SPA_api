4f4e8b99248720ab61e3b727b5e6308ec4f3f71b
Jonas <Jonas.burneika@gmail.com>
2019-04-01 21:15:24 +0300

    new route for possible prize list

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('prizes','PrizeController@index');
 Route::post('receipt','ReceiptsController@register');
